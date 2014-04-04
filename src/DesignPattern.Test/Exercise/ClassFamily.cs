520dabecad7dcd7262b05d3e487e5b185cfc28e5
arthor_cui <arthor.cui@gmail.com>
2014-04-04 18:46:37 +0800

    add clr exercise test

---
+++ src/DesignPattern.Test/Exercise/ClassFamily.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Test.Exercise
+{
+    public class ClassFamily
+    {
+        public void ClassTest()
+        {
+            var ia = new InternalA();
+            var pa = new PublicA();
+            var ib = new InternalB();
+            var pb = new PublicB();
+            var pc = new PublicC();
+
+            Console.WriteLine(ib._d);
+            pb.C();
+        }
+    }
+
+    internal class InternalA
+    {
+        protected const Int32 _a = 1;
+
+        protected void A()
+        {
+            Console.WriteLine("I`m protected method A for internal class A.");
+        }
+    }
+
+    public class PublicA
+    {
+        private const Int32 _b = 2;
+
+        private void B()
+        {
+            Console.WriteLine("I`m private method B for public class A.");
+        }
+    }
+
+    public class InternalB : InternalA
+    {
+        public readonly Int32 _d = 4;
+    }
+
+    public class PublicB : PublicA
+    {
+        internal const Int32 _c = 3;
+
+        public void C()
+        {
+            Console.WriteLine("I`m public method C for public class B.");
+        }
+    }
+
+    public class PublicC : InternalA
+    {
+        protected void D()
+        {
+            Console.WriteLine("I`m protected method D for public class C.");
+        }
+    }
+
+}
---
+++ src/DesignPattern.Test/Exercise/FrameworkDynamic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Test.Exercise
+{
+    public sealed class FrameworkDynamic
+    {
+        //嵌套类
+        private class SomeNestedType { }
+
+        //常量、只读和静态字段
+        private const Int32 SomeConstant = 1;
+        private readonly Int32 SomeReadOnlyField = 2;
+        private static Int32 SomeReadWriteField = 3;
+
+        //类型构造器
+        static FrameworkDynamic() { }
+
+        //实例构造器
+        public FrameworkDynamic(Int32 x) { }
+        public FrameworkDynamic() { }
+
+        //实例方法和静态方法
+        private String InstanceMethod()
+        {
+            return null;
+        }
+        private static void Main() { }
+
+        //实例属性
+        public Int32 SomeProperty
+        {
+            get { return 0; }
+            set { }
+        }
+
+        //实例有参属性（索引器）
+        public Int32 this[String s]
+        {
+            get { return 0; }
+            set { }
+        }
+
+        //实例事件
+        public event EventHandler SomeEvent;
+    }
+}
