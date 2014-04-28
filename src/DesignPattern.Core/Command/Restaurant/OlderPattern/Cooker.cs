be58b9e5093508f7d326e66466001bc698d2dbaa
arthor_cui <arthor.cui@gmail.com>
2014-04-28 08:05:38 +0800

    add singleton

---
+++ src/DesignPattern.Core/Command/Restaurant/OlderPattern/Cooker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Command.Restaurant
+{
+    public class Cooker
+    {
+        public void MakeBurger() { }
+
+        public void MakeShake() { }
+    }
+}
---
+++ src/DesignPattern.Core/Command/Restaurant/OlderPattern/Serve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Command.Restaurant
+{
+    public class Serve
+    {
+        private Cooker cook;
+
+        public void TakeOrder(List<Order> orderList) { }
+
+        public void OrderUp()
+        {
+            cook.MakeBurger();
+            cook.MakeShake();
+        }
+    }
+}
