a998cfc3c74d7da7b4d37e380f8f8d229e82ee60
arthor_cui <arthor.cui@gmail.com>
2014-05-09 23:47:21 +0800

    add template pattern hook method

---
+++ src/DesignPattern.Core/Template/StarbuzzExample/CaffeineBeverageWithHook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Template.StarbuzzExample
+{
+    public abstract class CaffeineBeverageWithHook
+    {
+        protected void PrepareRecipe()
+        {
+            this.BoilWater();
+            this.Brew();
+            this.PourInCup();
+            if (this.CustomerWantsCondiments())
+            {
+                this.AddCondiments();
+            }
+        }
+
+        public abstract void Brew();
+
+        public abstract void AddCondiments();
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling Water.");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup.");
+        }
+
+        public bool CustomerWantsCondiments()
+        {
+            return true;
+        }
+    }
+}
