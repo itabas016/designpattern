92a2b1feb6d132b954115f86b9c07a1e92e882ba
arthor_cui <arthor.cui@gmail.com>
2014-05-23 16:09:34 +0800

    add prototype pattern by real world example

---
+++ src/DesignPattern.Core/ProtoType/RealWorld/Color.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.ProtoType.RealWorld
+{
+    public class Color : ColorPrototype
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+
+        public Color(int red, int green, int blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}",Red, Green, Blue);
+            return (ColorPrototype)this.MemberwiseClone();
+
+        }
+    }
+}
--- src/DesignPattern.Test/ProtoTypeTest/ProtoTypeExampleTest.cs
+++ src/DesignPattern.Test/ProtoTypeTest/ProtoTypeExampleTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesignPattern.Core.ProtoType.RealWorld;
 using DesignPattern.Core.ProtoType.Structural;
 using Xunit;
 
@@ -20,6 +21,24 @@ namespace DesignPattern.Test.ProtoTypeTest
             ConcreteProtoType2 protoType2 = new ConcreteProtoType2("II");
             ConcreteProtoType2 c2 = (ConcreteProtoType2)protoType2.Clone();
             Console.WriteLine("Cloned: {0}", c2.Id);
+        }
+
+        [Fact]
+        public void RealWorldTest()
+        {
+            ColorManager colormanager = new ColorManager();
+
+            colormanager["red"] = new Color(255, 0, 0);
+            colormanager["green"] = new Color(0, 255, 0);
+            colormanager["blue"] = new Color(0, 0, 255);
+
+            colormanager["angry"] = new Color(255, 54, 0);
+            colormanager["peace"] = new Color(128, 211, 128);
+            colormanager["flame"] = new Color(211, 34, 20);
+
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
         }
     }
 }
