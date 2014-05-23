33b6f2b482d3a8dedb5d6be9e85ad7e1b4fc02da
arthor_cui <arthor.cui@gmail.com>
2014-05-23 14:34:05 +0800

    add singleton pattern sample example

---
+++ src/DesignPattern.Core/Singleton/Structural/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Singleton.Structural
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
---
+++ src/DesignPattern.Test/SingletonTest/SingletonExampleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPattern.Core.Singleton.Structural;
+using Xunit;
+
+namespace DesignPattern.Test.SingletonTest
+{
+    public class SingletonExampleTest
+    {
+        [Fact]
+        public void StructualTest()
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("The instance is same ,this is singleton pattern.");
+            }
+            else
+            {
+                Console.WriteLine("The instance is not same.");
+            }
+        }
+    }
+}
