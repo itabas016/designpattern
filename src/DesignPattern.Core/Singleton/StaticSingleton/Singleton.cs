b62de36575a4f7990ec5e4fbebc0523e3af105ec
arthor_cui <arthor.cui@gmail.com>
2014-06-05 23:11:53 +0800

    add static construct method instantiate singleton pattern

---
+++ src/DesignPattern.Core/Singleton/StaticSingleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Singleton.StaticSingleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance = new Singleton();
+
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
--- src/DesignPattern.Test/SingletonTest/SingletonExampleTest.cs
+++ src/DesignPattern.Test/SingletonTest/SingletonExampleTest.cs
@@ -25,5 +25,22 @@ namespace DesignPattern.Test.SingletonTest
                 Console.WriteLine("The instance is not same.");
             }
         }
+
+        [Fact]
+        public void StaticSingletonTest()
+        {
+            var s1 = DesignPattern.Core.Singleton.StaticSingleton.Singleton.Instance;
+
+            var s2 = DesignPattern.Core.Singleton.StaticSingleton.Singleton.Instance;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("The instance is same ,this is singleton pattern.");
+            }
+            else
+            {
+                Console.WriteLine("The instance is not same.");
+            }
+        }
     }
 }
