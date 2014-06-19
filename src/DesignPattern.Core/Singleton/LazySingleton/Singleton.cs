5c4c7e171391753db56cca470b0a5309fc378ee7
arthor_cui <arthor.cui@gmail.com>
2014-06-19 15:14:40 +0800

    add lazy singleton pattern and and generic singleton pattern

---
+++ src/DesignPattern.Core/Singleton/LazySingleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Singleton.LazySingleton
+{
+    public class Singleton
+    {
+        protected static class SingletonHandle
+        {
+            public static Singleton instance = new Singleton();
+        }
+
+        public static Singleton GetInstance()
+        {
+            return SingletonHandle.instance;
+        }
+    }
+}
---
+++ src/DesignPattern.Core/Singleton/StaticSingleton/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Core.Singleton.StaticSingleton
+{
+    public class Client : SingletonBase<Client>
+    {
+    }
+}
