62d5baa1d622eca12df5bb877851a7969fb3c969
arthor_cui <arthor.cui@gmail.com>
2014-10-31 14:16:55 +0800

    add tupple and yield test

---
+++ src/Function.Test/Common/TupleTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Function.Test.Common
+{
+    public class TupleTest
+    {
+        [Fact]
+        public void tuple_use_test()
+        {
+            Tuple<int> t1 = new Tuple<int>(1);
+
+            Tuple<int, int> t2 = Tuple.Create<int, int>(1, 3);
+
+            Tuple<int, Tuple<int, int>> t3 = new Tuple<int, Tuple<int, int>>(1, Tuple.Create<int, int>(1, 3));
+
+            Console.WriteLine(t1.Item1);
+            Console.WriteLine(t2.Item1 + t2.Item2);
+            Console.WriteLine(t3.Item1 + (t3.Item2.Item1 + t3.Item2.Item2));
+        }
+    }
+}
---
+++ src/Function.Test/Common/YieldTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Function.Test.Common
+{
+    public class YieldTest
+    {
+        [Fact]
+        public void yield_return_test()
+        {
+            int[] s = new int[2];
+            foreach (var item in List)
+            {
+                s[0] = item;
+                GetEnumerator();
+                s[1] = item;
+            }
+
+            Console.WriteLine(string.Format("{0}|{1}", s[0], s[1]));
+        }
+
+        private List<int> List = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        private static IEnumerator<int> GetEnumerator()
+        {
+            yield return 4;
+            yield return 8;
+        }
+    }
+}
