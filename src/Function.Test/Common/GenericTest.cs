2248d9199645f3fef259211cea948ba844992828
Roger.Cui <jesuie@live.cn>
2017-12-09 12:29:54 +0800

    add static object initinalzied test.

---
+++ src/Function.Test/Common/GenericTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Function.Test.Common
+{
+    public class GenericTest
+    {
+        [Fact]
+        public void ListvsArrayList()
+        {
+            var l1 = new List<String>();
+            ArrayList l2 = new ArrayList();
+
+            l2.Add(1);
+            l2.Add("I'm two");
+            l2.Add(new List<int> { 3, 4, 5 });
+
+            foreach (var item in l2)
+            {
+                Console.WriteLine(item);
+            }
+
+            l2.RemoveAt(0);
+            l2.Insert(0, "1");
+
+            Console.WriteLine(l2[0]);
+            Console.WriteLine("The type is " + l2[0].GetType());
+
+        }
+    }
+}
---
+++ src/Function.Test/Common/StaticObjectTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Function.Test.Common
+{
+    public class StaticObjectTest
+    {
+        public class Device : BaseDevice
+        {
+            static SerialNumber serialNumber = new SerialNumber("X-100000");
+            static string model = "X";
+            static Device()
+            {
+                Console.WriteLine("Device static constructor.");
+                Console.WriteLine("Device Info: serialNumber: [{0}], model: [{1}]", serialNumber.ToString(), model);
+            }
+            public Device()
+            {
+                Console.WriteLine("Device constructor.");
+            }
+
+        }
+
+        public class BaseDevice
+        {
+            static SerialNumber serialNumber = new SerialNumber();
+            static BaseDevice()
+            {
+                Console.WriteLine("Base Device static constructor.");
+                Console.WriteLine("Base Device Info: serialNumber: [{0}]", serialNumber);
+            }
+            public BaseDevice()
+            {
+                Console.WriteLine("Base Device constructor.");
+            }
+        }
+
+        public class SerialNumber
+        {
+            public string _serialNumber;
+            public SerialNumber(string serialNumber = "000000")
+            {
+                _serialNumber = serialNumber;
+                Console.WriteLine("Initialize serialNumber: {0}", serialNumber);
+            }
+            public override string ToString()
+            {
+                return _serialNumber;
+            }
+        }
+
+        [Fact]
+        public void FieldInitinalizeTest()
+        {
+            var device = new Device();
+            Console.Read();
+        }
+    }
+}
